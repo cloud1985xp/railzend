e9f87943191c791afaa2c81cffe97714e2bec1b9
Aaron Kuo <atk@mini.olivinesoft.com>
2012-02-03 09:30:19 +0800

    update document ready

--- lib/railzend/view/helper/document_ready.rb
+++ lib/railzend/view/helper/document_ready.rb
@@ -2,15 +2,14 @@ module Railzend
   module View
     module Helper
       class DocumentReady
-        @scripts = []
-        def scripts
-          @scripts
+        @@scripts = []
+        def self.append( script )
+          @@scripts << script
+          self
         end
-        def document_ready( script )
-          scripts << script
-        end
-        def render_document_ready
-          s = scripts.join("\n")
+        def self.to_s
+          s = @@scripts.join("\n")
+          @@scripts = []
           "<script>$(document).ready(function(){#{s}})</script>".html_safe
         end
       end
